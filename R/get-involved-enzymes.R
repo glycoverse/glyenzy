eef96f1574711096238a1de6ba6171cd0c89c165
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2025-08-07 15:56:53 +0800

    feat: add `get_involved_enzymes()`

---
+++ R/get-involved-enzymes.R
@@ -0,0 +1,48 @@
+#' Get Involved Enzymes
+#'
+#' This function identifies all enzymes involved in the biosynthesis of a glycan.
+#' Note that the result can be misleading if the glycan is not intact.
+#'
+#' @param glycans A [glyrepr::glycan_structure()], or a character vector of
+#'   glycan structure strings supported by [glyparse::auto_parse()].
+#'
+#' @return A list of character vectors, each containing the names of enzymes
+#'   involved in the biosynthesis of the corresponding glycan.
+#'
+#' @examples
+#' library(glyrepr)
+#' library(glyparse)
+#'
+#' # Use `glycan_structure()`
+#' glycans <- auto_parse(c(
+#'   "GlcNAc(b1-2)Man(a1-3)[Man(a1-6)]Man(b1-4)GlcNAc(b1-4)GlcNAc(b1-",
+#'   "GlcNAc(b1-2)Man(a1-3)[GlcNAc(b1-2)Man(a1-6)]Man(b1-4)GlcNAc(b1-4)GlcNAc(b1-"
+#' ))
+#' get_involved_enzymes(glycans)
+#'
+#' # Or use characters directly
+#' get_involved_enzymes("GlcNAc(b1-2)Man(a1-3)[Man(a1-6)]Man(b1-4)GlcNAc(b1-4)GlcNAc(b1-")
+#'
+#' @export
+get_involved_enzymes <- function(glycans) {
+  if (is.character(glycans)) {
+    glycans <- glyparse::auto_parse(glycans)
+  } else if (!glyrepr::is_glycan_structure(glycans)) {
+    cli::cli_abort(c(
+      "{.arg glycans} must be a {.cls glyrepr_structure} vector or a character vector of glycan structure strings.",
+      "x" = "Got {.cls {class(glycans)}}."
+    ))
+  }
+
+  masks <- purrr::map(glyenzy_enzymes, ~ .safe_is_synthesized_by(glycans, .x))
+  mast_mat <- do.call(cbind, masks)
+  purrr::map(seq_along(glycans), ~ names(glyenzy_enzymes)[mast_mat[.x, ]])
+}
+
+# Like `.is_synthesized_by()`, but returns FALSE instead of throwing error.
+.safe_is_synthesized_by <- function(glycans, enzyme) {
+  tryCatch(
+    .is_synthesized_by(glycans, enzyme),
+    error = function(e) rep(FALSE, length(glycans))
+  )
+}
---
+++ tests/testthat/test-get-involved-enzymes.R
@@ -0,0 +1,23 @@
+# ===== Input type =====
+test_that("get_involved_enzymes works for `glyrepr_structure`", {
+  glycan <- glyparse::parse_iupac_condensed("GlcNAc(b1-2)Man(a1-3)[Man(a1-6)]Man(b1-4)GlcNAc(b1-4)GlcNAc(b1-")
+  expect_true("MGAT1" %in% get_involved_enzymes(glycan)[[1]])
+})
+
+test_that("get_involved_enzymes works for characters", {
+  glycan <- "GlcNAc(b1-2)Man(a1-3)[Man(a1-6)]Man(b1-4)GlcNAc(b1-4)GlcNAc(b1-"
+  expect_true("MGAT1" %in% get_involved_enzymes(glycan)[[1]])
+})
+
+test_that("get_involved_enzymes works for vectorized inputs", {
+  glycans <- c(
+    "GlcNAc(b1-2)Man(a1-3)[Man(a1-6)]Man(b1-4)GlcNAc(b1-4)GlcNAc(b1-",
+    "GlcNAc(b1-2)Man(a1-3)[GlcNAc(b1-2)Man(a1-6)]Man(b1-4)GlcNAc(b1-4)GlcNAc(b1-"
+  )
+  expect_false("MGAT2" %in% get_involved_enzymes(glycans)[[1]])
+  expect_true("MGAT2" %in% get_involved_enzymes(glycans)[[2]])
+})
+
+test_that("get_involved_enzymes rejects invalid inputs", {
+  expect_error(get_involved_enzymes(123), "`glycans` must be")
+})
